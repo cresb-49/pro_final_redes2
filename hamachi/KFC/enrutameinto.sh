99c3047308936689355c1124ffa550d1e517864b
Benjamin <carlosbpac@gmail.com>
2024-10-08 23:39:39 -0600

    Refactor firewall routing configuration and add rules for routing traffic between PC2 and the internet

--- hamachi/KFC/enrutameinto.sh
+++ hamachi/KFC/enrutameinto.sh
@@ -7,13 +7,13 @@ sudo ip route add 192.128.25.0/24 via 11.11.11.1 dev enx00e04c360afb
 sudo iptables -t nat -A POSTROUTING -o wlp0s20f3 -j MASQUERADE
 
 
-# Permitir tráfico desde PC2 (enp1s0) hacia internet
-sudo iptables -A FORWARD -i enp1s0 -o wlp0s20f3 -j ACCEPT
-# Permitir las respuestas desde internet hacia PC2
-sudo iptables -A FORWARD -i wlp0s20f3 -o enp1s0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-
 # ALTERNATICA EN CASO DE FALLO
 # Permitir tráfico desde PC2 (enp1s0) hacia internet
-# sudo iptables -A FORWARD -i enx00e04c360afb -o wlp0s20f3 -j ACCEPT
+#sudo iptables -A FORWARD -i enp1s0 -o wlp0s20f3 -j ACCEPT
 # Permitir las respuestas desde internet hacia PC2
-# sudo iptables -A FORWARD -i wlp0s20f3 -o enx00e04c360afb -m state --state RELATED,ESTABLISHED -j ACCEPT
+#sudo iptables -A FORWARD -i wlp0s20f3 -o enp1s0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+# Permitir tráfico desde PC2 (enp1s0) hacia internet
+sudo iptables -A FORWARD -i enx00e04c360afb -o wlp0s20f3 -j ACCEPT
+# Permitir las respuestas desde internet hacia PC2
+sudo iptables -A FORWARD -i wlp0s20f3 -o enx00e04c360afb -m state --state RELATED,ESTABLISHED -j ACCEPT
