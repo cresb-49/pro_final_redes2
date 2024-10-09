7c0b6fd1537910e077e6e05f2515b5c13b6b5c05
Benjamin <carlosbpac@gmail.com>
2024-10-08 23:23:35 -0600

    Refactor firewall routing configuration

--- firewall/KFC/enrutameinto.sh
+++ firewall/KFC/enrutameinto.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# En PC2 (Firewall)
-sudo iptables -t nat -A POSTROUTING -o enp1s0 -j MASQUERADE
+# # En PC2 (Firewall)
+# sudo iptables -t nat -A POSTROUTING -o enp1s0 -j MASQUERADE
 
-# Permitir tráfico desde PC1 (enx00e04c360714) hacia PC3 (enp1s0)
-sudo iptables -A FORWARD -i enx00e04c360714 -o enp1s0 -j ACCEPT
-# Permitir las respuestas desde PC3 (enp1s0) hacia PC1 (enx00e04c360714)
-sudo iptables -A FORWARD -i enp1s0 -o enx00e04c360714 -m state --state RELATED,ESTABLISHED -j ACCEPT
+# # Permitir tráfico desde PC1 (enx00e04c360714) hacia PC3 (enp1s0)
+# sudo iptables -A FORWARD -i enx00e04c360714 -o enp1s0 -j ACCEPT
+# # Permitir las respuestas desde PC3 (enp1s0) hacia PC1 (enx00e04c360714)
+# sudo iptables -A FORWARD -i enp1s0 -o enx00e04c360714 -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 # Eliminamos la default gateway
 sudo ip route del default
