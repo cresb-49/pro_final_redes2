46cfaf41936ba4c6a3c48899c61aff02e55ade80
Benjamin <carlosbpac@gmail.com>
2024-10-16 20:08:53 -0600

    Refactor firewall routing configuration and remove unnecessary rules

--- firewall/KFC/enrutameinto.sh
+++ firewall/KFC/enrutameinto.sh
@@ -4,9 +4,9 @@
 #sudo iptables -t nat -A POSTROUTING -o enp1s0 -j MASQUERADE
 
 # Permitir tráfico desde PC1 (enx00e04c360714) hacia PC3 (enp1s0)
-sudo iptables -A FORWARD -i enx00e04c360714 -o enp1s0 -j ACCEPT
+#sudo iptables -A FORWARD -i enx00e04c360714 -o enp1s0 -j ACCEPT
 # Permitir las respuestas desde PC3 (enp1s0) hacia PC1 (enx00e04c360714)
-sudo iptables -A FORWARD -i enp1s0 -o enx00e04c360714 -m state --state RELATED,ESTABLISHED -j ACCEPT
+#sudo iptables -A FORWARD -i enp1s0 -o enx00e04c360714 -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 # Eliminamos la default gateway
 sudo ip route del default
@@ -14,6 +14,6 @@ sudo ip route del default
 sudo ip route add default via 11.11.11.2 dev enp1s0
 
 #Configuracion para wordpress
-sudo iptables -t nat -A POSTROUTING -o enx00e04c360714 -j MASQUERADE
-sudo iptables -A FORWARD -i enp1s0 -o enx00e04c360714 -j ACCEPT
-sudo iptables -A FORWARD -i enx00e04c360714 -o enp1s0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+#sudo iptables -t nat -A POSTROUTING -o enx00e04c360714 -j MASQUERADE
+#sudo iptables -A FORWARD -i enp1s0 -o enx00e04c360714 -j ACCEPT
+#sudo iptables -A FORWARD -i enx00e04c360714 -o enp1s0 -m state --state RELATED,ESTABLISHED -j ACCEPT
