b617828a74f579e53158935cd5bb6cc8c5dc945a
MenWic <lamr467@gmail.com>
2024-10-18 20:03:49 +0000

    Scripts de re-direccionamiento de DNS para Cliente y Firewall

---
+++ firewall/config.sh
@@ -0,0 +1,10 @@
+#APAGAR INTERFAZ enp1s0 (por ahi resolvia el DNS)
+sudo ip link set enp1s0 down
+
+
+#SERVIDOR DNS
+cat << EOL | sudo tee /etc/resolv.conf > /dev/null
+nameserver=8.8.8.8
+nameserver=8.8.4.4
+
+EOL
