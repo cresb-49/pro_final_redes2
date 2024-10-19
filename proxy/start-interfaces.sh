1094832d430ddcd6d4a5caddb430eb7237d12610
MenWic <lamr467@gmail.com>
2024-10-19 16:58:29 +0000

    Actualizacion de proxy/enrutamiento.sh

--- proxy/start-interfaces.sh
+++ proxy/start-interfaces.sh
@@ -11,12 +11,18 @@ iface lo inet loopback
 #Red Proxy-FW
 auto enp3s0
 iface enp3s0 inet static
-#  address 192.168.1.2/30
-  address 11.11.11.2/30
+  address 192.168.1.2/30
+#  address 11.11.11.2/30
 EOL
+
+
+
+#REINICIO DE INTERFACES
+systemctl restart networking.service
+
 
 
 #RUTAS O VIAS
 #Ruta Admin-FW
-#ip route add 192.168.100.0/24 via 192.168.1.1 dev enp3s0
-ip route add 192.168.25.0/24 via 11.11.11.1 dev enp3s0
+ip route add 192.168.100.0/24 via 192.168.1.1 dev enp3s0
+#ip route add 192.168.25.0/24 via 11.11.11.1 dev enp3s0
