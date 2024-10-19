29e622944669ea16f5ab840785e37d53cca7258a
Caronte-Aqueronte <elrincondelgamer77@gmail.com>
2024-10-19 18:05:04 -0500

    Actualizacion del cliente/config.sh

--- cliente/config.sh
+++ cliente/config.sh
@@ -4,7 +4,7 @@ sudo ip link set enp1s0 down
 
 #SERVIDOR DNS
 cat << EOL | sudo tee /etc/resolv.conf > /dev/null
-nameserver=8.8.8.8
-nameserver=8.8.4.4
+nameserver 8.8.8.8
+nameserver 8.8.4.4
 
 EOL
